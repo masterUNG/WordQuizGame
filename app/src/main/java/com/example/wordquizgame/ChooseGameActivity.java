6869ecec0a7f81a1fd4dd3ded2191d1e2264c4a1
masterUNG <phrombutr@gmail.com>
2015-12-29 10:52:13 +0700

    Intent to ShowGame finish

--- app/src/main/java/com/example/wordquizgame/ChooseGameActivity.java
+++ app/src/main/java/com/example/wordquizgame/ChooseGameActivity.java
@@ -1,7 +1,9 @@
 package com.example.wordquizgame;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseGameActivity extends ActionBarActivity {
 
@@ -9,5 +11,12 @@ public class ChooseGameActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_game);
+    }   // Main Method
+
+    public void clickChooseGame(View view) {
+        Intent objIntent = new Intent(ChooseGameActivity.this, ShowGameActivity.class);
+        startActivity(objIntent);
     }
-}
+
+
+}   // Main Class
