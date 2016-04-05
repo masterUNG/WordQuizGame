1771b921a157c31a15d5b1181bf06bf036627924
masterUNG <phrombutr@gmail.com>
2016-04-05 10:24:08 +0700

    clear TextView Finish

--- app/src/main/java/com/example/wordquizgame/Game3Activity.java
+++ app/src/main/java/com/example/wordquizgame/Game3Activity.java
@@ -1,13 +1,52 @@
 package com.example.wordquizgame;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Game3Activity extends ActionBarActivity {
+
+    //Explicit
+    private ImageView imageView;
+    private TextView textView;
+    private LinearLayout linearLayout;
+    private int[] questionImageInts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game3);
-    }
-}
+
+        //Bind Widget
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.textView3);
+        linearLayout = (LinearLayout) findViewById(R.id.linButton);
+
+        //Get Data
+        MyDataImage myDataImage = new MyDataImage();
+        questionImageInts = myDataImage.questionImageInts;
+
+        //Show Question Image
+        showQuestionImage(0);
+
+        //Clear Answer
+        clearAnswer();
+
+
+    }   // Main Method
+
+    private void clearAnswer() {
+
+        textView.setText("");
+
+    }   // clearAnswer
+
+    private void showQuestionImage(int intIndex) {
+
+        imageView.setImageResource(questionImageInts[intIndex]);
+
+    }   // showQuestion
+
+}   // Main Class
---
+++ app/src/main/java/com/example/wordquizgame/MyDataImage.java
@@ -0,0 +1,31 @@
+package com.example.wordquizgame;
+
+/**
+ * Created by masterUNG on 4/5/16 AD.
+ */
+public class MyDataImage {
+
+    //Explicit
+    public int[] questionImageInts = {R.drawable.animals_cat,
+            R.drawable.animals_dog,
+            R.drawable.animals_dolphin,
+            R.drawable.body_arm,
+            R.drawable.body_ear,
+            R.drawable.body_eye,
+            R.drawable.body_foot,
+            R.drawable.body_hair,
+            R.drawable.body_hand,
+            R.drawable.body_mouth};
+
+    public String[] answerStrings = {"cat",
+            "dog",
+            "dolphin",
+            "arm",
+            "ear",
+            "eye",
+            "foot",
+            "hair",
+            "hand",
+            "mouth"};
+
+}   // Main Class
